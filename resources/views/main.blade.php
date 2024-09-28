4f2e7f1681a12a07a509e04c6a442b699eefbcd4
Mag. Dr. Stefan Krejci <45850599+steve9825679@users.noreply.github.com>
2024-09-28 15:55:25 +0200

    wip

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+<x-guest-layout>
+    <div class="py-6">
+        <div class="max-w-full h-full flex flex-col mx-auto sm:px-4 lg:px-6">
+            <div class="min-h-screen bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="h-56 text-center text-6xl text-pink-800 bg-slate-500">
+                    VSOhlsdorf App
+                </div>
+                <div class="flex-1 text-right mt-16 text-xl text-amber-600 bg-slate-800 font-semibold">
+                    <a href="{{route('main')}}">Hereinspaziert &longrightarrow;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-guest-layout>
