2570112c87ed4c4643730cfc74b0be228a4dac2c
Mag. Dr. Stefan Krejci <45850599+steve9825679@users.noreply.github.com>
2024-09-28 16:25:27 +0200

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+import { createApp } from 'vue';
+import Calculator from './components/calculator/Caculator.vue';
+ 
+createApp({})
+  .component('calculator', Calculator)
+  .mount('#app')
