f38895e414bda25645bf46b9c92b57aa01e0e021
Mag. Dr. Stefan Krejci <45850599+steve9825679@users.noreply.github.com>
2024-09-28 16:13:10 +0200

    wip

---
+++ resources/views/components/calculator.blade.php
@@ -0,0 +1,11 @@
+<div class="flex justify-between">
+    <div class="h-64 w-64 border-amber-300 border-4 rounded-lg p-2">
+        Calculator
+    </div>
+    <div class="h-full grid self-center text-4xl text-pink-400">
+        Das kleine 1x1
+    </div>
+    <div class="h-64 w-64 border-amber-300 border-4 rounded-lg p-2">
+        Results
+    </div>
+</div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,15 +1,13 @@
-<x-guest-layout>
+<x-public>
     <div class="py-6">
-        <div class="max-w-full h-full flex flex-col mx-auto sm:px-4 lg:px-6">
-            <div class="min-h-screen bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="h-56 text-center text-6xl text-pink-800 bg-slate-500">
-                    VSOhlsdorf App
-                </div>
-                <div class="flex-1 text-right mt-16 text-xl text-amber-600 bg-slate-800 font-semibold">
-                    <a href="{{route('main')}}">Hereinspaziert &longrightarrow;</a>
-                </div>
+        <div class="max-w-full h-full flex flex-col mx-auto">
+            <div class="text-center text-6xl py-4 text-pink-800">
+                VSOhlsdorf App
+            </div>
+            <div>
+                <x-calculator></x-calculator>
             </div>
         </div>
     </div>
 
-</x-guest-layout>
+</x-public>
