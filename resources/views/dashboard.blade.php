9d220028359255042e31ae25be4558566327329a
Mag. Dr. Stefan Krejci <45850599+steve9825679@users.noreply.github.com>
2024-09-28 09:57:18 +0200

    install breeze

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -9,12 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="text-center text-4xl text-pink-600">
-                        Bettina App
-                    </div>
-                    <div class="text-pink-400">aha</div>
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
-</x-guest-layout>
+</x-app-layout>
