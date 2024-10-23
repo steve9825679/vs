9173b4355c4818b7dc8dc0ffe1537c9f4aa5dfd6
Lucas Höglinger <lucas.hoeglinger@flowvision.dev>
2024-10-23 10:10:26 +0200

    Added modal view for correct and false also added roundCounter

--- resources/js/components/lucas/Board100.vue
+++ resources/js/components/lucas/Board100.vue
@@ -3,7 +3,12 @@ import { ref, onMounted } from 'vue';
 
 let randNumber = ref(0);
 
+let round = ref(1)
+
 const numbers = ref(Array.from({ length: 100 }, (_, i) => i + 1));
+let ok = ref(false)
+
+let nok = ref(false)
 
 const randomNumber = (min, max) => {
   randNumber.value = Math.floor(Math.random() * (max - min + 1) + min);
@@ -12,12 +17,19 @@ const randomNumber = (min, max) => {
 function generateAddNew() {
     randomNumber(1, 100)
 }
-function handleClick(number) {
+function checkNumber(number) {
   if (number === randNumber.value) {
-    alert("Correct");
+    ok.value = true
+    round.value++
     randomNumber(1, 100)
+    setTimeout(() => {
+      ok.value = false
+    }, 2000)
   } else {
-    alert("Wrong, try again!");
+    nok.value = true
+    setTimeout(() => {
+      nok.value = false
+    }, 2000)
   }
 }
 
@@ -28,10 +40,17 @@ onMounted(() => {
 <template>
   <div id="app">
     <div>
-        Clicken Sie {{randNumber}};
+        <h1 class="number">Drücke auf {{randNumber}};</h1>
+        <h1 >Runde {{ round }}</h1>
+        <div class="text-4xl bg-green-700 text-white rounded-md py-3 px-8" v-show="ok">
+                    Suuuuuuuuper 👍
+                </div>
+                <div class="text-4xl bg-slate-700 text-white rounded-md py-3 px-8" v-show="nok">
+                    Oiiiiiiii 😒
+                </div>
     </div>
     <div class="grid">
-      <div v-for="number in numbers" :key="number" @click="handleClick(number)" class="grid-item">
+      <div v-for="number in numbers" :key="number" @click="checkNumber(number)" class="bg-amber-200 text-amber-800 font-bold border-b-4 cursor-pointer grid-item">
         {{ number }}
       </div>
     </div>
@@ -44,14 +63,17 @@ onMounted(() => {
   margin-top: 50px;
 }
 
+.number{
+  font-size: 2em;
+}
+
 .grid {
   display: grid;
-  grid-template-columns: repeat(10, 1fr); /* 10 columns */
+  grid-template-columns: repeat(10, 1fr);
   gap: 5px;
 }
 
 .grid-item {
-  background-color: #f0f0f0;
   border: 1px solid #ccc;
   padding: 10px;
   cursor: pointer;
@@ -61,4 +83,3 @@ onMounted(() => {
   background-color: #ddd;
 }
 </style>
->
