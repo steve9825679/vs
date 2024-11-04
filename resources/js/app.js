b845c73af05e6cb4f3b4923c2673ec8b0889fc54
Sim <simeon.nieuwenhuys7@gmail.com>
2024-11-04 14:38:29 +0100

    idk what changed

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,10 @@ Alpine.start();
 
 import { createApp } from 'vue';
 import Calculator from './components/calculator/Caculator.vue';
+import CarryOverTen from './components/calculator/simeon/CarryOverTen.vue';
  
 createApp({})
   .component('calculator', Calculator)
+  .component('carry-over-ten', CarryOverTen)  
   .mount('#app')
+  
--- resources/views/components/calculator.blade.php
+++ resources/views/components/calculator.blade.php
@@ -1,1 +1,1 @@
-<calculator></calculator>
+<carry-over-ten><carry-over-ten/>
