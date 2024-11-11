724ebdf90fcc8e2e3cb929339f7cb4d7b23f3df6
Sim <simeon.nieuwenhuys7@gmail.com>
2024-11-11 14:53:25 +0100

    carry over ten done

--- resources/js/components/GameSelector.vue
+++ resources/js/components/GameSelector.vue
@@ -35,6 +35,7 @@ import MathChallengeSquare from './krs/MathChallengeSquare.vue';
 import MultipleGame from './krs/MultipleGame.vue';
 import VisualMultipleGame from './krs/VisualMultipleGame.vue';
 import MultiplierSameGame from './krs/MultiplierSameGame.vue';
+import CarryOverTen from './simeon/CarryOverTen.vue';
 
 export default {
   setup() {
@@ -48,6 +49,7 @@ export default {
       { name: 'MultipleGame', label: 'Reihen mit Fehler', component: MultipleGame },
       { name: 'VisualMultipleGame', label: 'Visuelles Multiplizieren', component: VisualMultipleGame },
       { name: 'MultiplierSameGame', label: 'Gleiche Multiplikationen finden', component: MultiplierSameGame },
+      { name: 'CarryOverTen', label: '10er Überschreiten', component: CarryOverTen },
     ];
 
     const selectedGame = ref(games[0].name);
